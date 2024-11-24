dec36c57d81fde1ce1bd8242751d31cd35cfb7b5
itama <itamarbabai98@gmail.com>
2024-11-24 17:08:08 +0200

    corner strategy not working well, need to understand how to use bomb and unflippable.

--- WinnerAI.java
+++ WinnerAI.java
@@ -7,14 +7,18 @@ public class WinnerAI extends AIPlayer{
     ArrayList <Position> arr = new ArrayList<>();
     ArrayList<Position> corners_Arr = new ArrayList<>();
     public int move_counter;
+    public int co;
     public WinnerAI(boolean isPlayerOne) {
         super(isPlayerOne);
         this.isPlayerOne=isPlayerOne;
-        this.move_counter=12;
+        this.move_counter=8;
+        this.co=0;
+
     }
 
     @Override
     public Move makeMove(PlayableLogic gameStatus) {
+        System.out.println(co++);
     //take the corners and slide around
     //start with small flips 10 first moves
     //to be centered around opponent discs.
@@ -36,33 +40,38 @@ public class WinnerAI extends AIPlayer{
 
         Position corner =openCorner(gameStatus.ValidMoves()); //check if there is an available corner
         Position round_end=roundEnd(gameStatus.ValidMoves());
-        Position p=cornerStrategy(gameStatus.ValidMoves());
-        if (corner!=null){
+       // Position p=cornerStrategy(gameStatus.ValidMoves());
+        if (corner!=null){              //take the corner
             corners_Arr.add(corner);
             Disc disc = new SimpleDisc(this);
             Move move = new Move(disc,corner);
             move_counter--;
             return move;
-        } else if (!corners_Arr.isEmpty()& p!= null) {
-                Disc disc = new SimpleDisc(this);
-                Move move = new Move(disc,p);
+        }   else if (round_end!=null & this.number_of_unflippedable>0) {
+            Disc disc;
+                disc = new UnflippableDisc(this);
+                Move move = new Move(disc, round_end);
                 move_counter--;
                 return move;
-//        }  else if (round_end!=null) {
-//            Disc disc;
-//            if (this.number_of_unflippedable>0) {
-//                disc = new UnflippableDisc(this);
-//                Move move = new Move(disc, round_end);
-//                move_counter--;
-//                return move;
-//            }
-        } else if (move_counter>0) {
+        } else if (move_counter>=3) {
             Position pos=new Position(arr.getFirst().row(),arr.getFirst().col());
             Disc disc = new SimpleDisc(this);
             Move move = new Move(disc,pos);
             move_counter--;
             return move;
+        } else if (this.number_of_bombs>0&move_counter>=0) {
+                Position pos = new Position(arr.getFirst().row(), arr.getFirst().col());
+                Disc disc = new BombDisc(this);
+                Move move = new Move(disc, pos);
+                move_counter--;
+                return move;
         }
+//        else if (!corners_Arr.isEmpty()& p!= null) {
+//            Disc disc = new SimpleDisc(this);
+//            Move move = new Move(disc,p);
+//            move_counter--;
+//            return move;
+//        }
         Position greedyPos=new Position(arr.getLast().row(),arr.getLast().col());
             Disc disc = new SimpleDisc(this);
             Move move = new Move(disc,greedyPos);
@@ -95,31 +104,32 @@ public class WinnerAI extends AIPlayer{
         for ( int i=0;i<arr.size();i++){ //in this point we cant ce on the corner
             int x=arr.get(i).row();
             int y=arr.get(i).col();
-            if(x==0||x==7||y==0||y==7 ){
+           //
+            if(x==0||x==7||y==0||(y==7)){
                 return arr.get(i);
             }
         }
         return null;
 
     }
-    public Position cornerStrategy(List <Position> arr){
-        int[][] array =  {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-//        for (int i=0; i<corners_Arr.size(); i++){
-//            Position p=corners_Arr.get(i);
-//            for (int j=0 ; j<8 ;j++) {
-//                int x = p.row() + array[j][0], y = p.col() + array[j][1];
-//                Position p1 = new Position(x, y);
-//
-//                if ( GameLogic.isInBounds(p1)) {
-//                    for (int k=0;k<arr.size();k++){
-//                        if (arr.get(k).row()==x & arr.get(k).col()==y){
-//                            corners_Arr.add(p1);
-//                            return p1;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-        return null;
-    }
+//    public Position cornerStrategy(List <Position> arr){
+//        int[][] array =  {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+////        for (int i=0; i<corners_Arr.size(); i++){
+////            Position p=corners_Arr.get(i);
+////            for (int j=0 ; j<8 ;j++) {
+////                int x = p.row() + array[j][0], y = p.col() + array[j][1];
+////                Position p1 = new Position(x, y);
+////
+////                if ( GameLogic.isInBounds(p1)) {
+////                    for (int k=0;k<arr.size();k++){
+////                        if (arr.get(k).row()==x & arr.get(k).col()==y){
+////                            corners_Arr.add(p1);
+////                            return p1;
+////                        }
+////                    }
+////                }
+////            }
+////        }
+//        return null;
+//    }
 }
